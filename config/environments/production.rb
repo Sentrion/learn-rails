4aa59d0bd91f33914c8aff73545f611ff75b395e
Jean Nguyen <sentrion@gmail.com>
2014-01-31 05:21:07 +0000

    typo #2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ LearnRails::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-    address: "smtp:gmail.com",
+    address: "smtp.gmail.com",
     port: 587,
     domain: ENV["DOMAIN_NAME"],
     authentication: "plain",
